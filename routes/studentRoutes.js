5acfc48611a0a9e3a267e7d2278187fb5de5b5aa
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-04-10 00:48:45 +0530

    added json data with  mongoose from a local file - without droping/uploading json file manually

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -11,4 +11,15 @@ router.get("/all", async (req, res) => {
   res.send(allStudents);
 });
 
+//added data from a json file-to database-with .insertMany()
+router.post("/insert-all", async (req, res) => {
+  const collection = mongoose.connection.collection("mongo-students");
+
+  let testData = require("../playground/test.json");
+
+  let result = await collection.insertMany(testData);
+
+  res.send(result);
+});
+
 module.exports = router;
